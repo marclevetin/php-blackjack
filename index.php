2b238be01e82c95038faef96133ba15fabed15ed
marclevetin <marc@derbyhat.org>
2018-04-06 11:39:50 -0400

    added pseudocode defining project

---
+++ index.php
@@ -0,0 +1,24 @@
+<?php
+// pseudocode
+// 1. Initialize game.
+// 2. Get player names.  Add dealer to the end.
+// 3. Shuffle deck.
+// 4. Deal two cards to each player.
+      // If any player has Blackjack, they win.
+// 5. Player turn: ask if they want to hit or stay
+//    If hit, give them a card.
+//    If stay, move to next player.
+//    If total is over 21, player loses.
+// 6. Dealer turn:
+//    Dealer must hit until their total is >=17.
+// 7. Win?
+//    If a player's score is greater than the dealer's, they win.
+
+// Blackjack = Initial deal is 1 Ace and 1 10 or facecard.
+// Cards values = value printed on card; Ace is 1 or 11; Face cards are a 10.
+
+// Out of scope for now, future possibilities:
+// Splitting a hand
+// Multiple decks in the shoe
+// Betting
+// GUI
